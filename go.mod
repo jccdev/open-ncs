7a3ba18f0921610e9324864c9dfc46100aa38dc7
jccdev <jcastillodev@gmail.com>
2021-11-20 13:49:17 -0600

    Initial App Setup - Setup Go server code - Setup React ui

---
+++ go.mod
@@ -0,0 +1,5 @@
+module jcc.dev/open-ncs
+
+go 1.16
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
---
+++ ui/src/App.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Open Source NoCode Server/i);
+  expect(linkElement).toBeInTheDocument();
+});
