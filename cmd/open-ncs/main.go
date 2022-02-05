02f9c2c6deaa180e80cb27fde795f62cf0354f9c
jccdev <jcastillodev@gmail.com>
2022-02-04 18:21:35 -0600

    Initial UI setup

--- cmd/open-ncs/main.go
+++ cmd/open-ncs/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/jackc/pgx/v4"
 	"github.com/julienschmidt/httprouter"
 )
 
@@ -16,7 +19,21 @@ func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
 }
 
+func setupDb() *pgx.Conn {
+	// urlExample := "postgres://username:password@localhost:5432/database_name"
+	conn, err := pgx.Connect(context.Background(), os.Getenv("OPEN_NCS_DB"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	return conn
+}
+
 func main() {
+	conn := setupDb()
+
 	router := httprouter.New()
 	router.GET("/", Index)
 	router.GET("/hello/:name", Hello)
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module jcc.dev/open-ncs
 
 go 1.16
 
-require github.com/julienschmidt/httprouter v1.3.0 // indirect
+require (
+	github.com/jackc/pgx/v4 v4.14.0 // indirect
+	github.com/julienschmidt/httprouter v1.3.0
+)
